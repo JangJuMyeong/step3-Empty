21271cf4f89b9296a14e44003dc2d3a522d7d30e
JangJuMyeong <isouvezz@gmail.com>
2021-06-08 23:23:28 +0900

    finish practice

--- RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
+++ RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -7,17 +7,93 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 class MenuListViewModel {
     
-    let menus : [Menu] = [
-        Menu(name: "가지튀김", price: 2000, count: 0),
-        Menu(name: "오징어튀김", price: 2000, count: 0),
-        Menu(name: "새우튀김", price: 2000, count: 0),
-        Menu(name: "고구마튀김", price: 2000, count: 0)
-    ]
+//    var menus : [Menu] = [
+//        Menu(name: "가지튀김", price: 2000, count: 0),
+//        Menu(name: "오징어튀김", price: 2000, count: 0),
+//        Menu(name: "새우튀김", price: 2000, count: 0),
+//        Menu(name: "고구마튀김", price: 2000, count: 0)
+//    ]
+//
     
-    let itemsCount : Int = 5
-    let totalPrice : Int = 10000
+//    lazy var menuObservable = PublishSubject<[Menu]>() // 초기모델을 가져야한다면 BehaborSubject로 해야만한다.
+    var menuObservable = BehaviorSubject<[Menu]>(value: [])
+    lazy var itemsCount = menuObservable.map { $0.map { $0.count }.reduce(0, +)}
+//    var totalPrice : PublishSubject<Int> = PublishSubject()
+    lazy var totalPrice = menuObservable.map { $0.map { $0.price * $0.count }.reduce(0, +)}
+    
+    //Subject은 여러가지의 종류가있다.
+    //Observable처럼 값을 받을수도있지만 수정할수도 있다.
+    
+    init() {
+        
+        _ = APIService.fetchAllMenusRx()
+            .map{ data -> [MenuItem] in
+                struct Response: Decodable {
+                    let menus: [MenuItem]
+                }
+                let resopone = try! JSONDecoder().decode(Response.self, from: data)
+                
+                return resopone.menus
+
+            }
+            .map { menuItems -> [Menu] in
+                var menus : [Menu] = []
+                menuItems.enumerated().forEach { (index , item) in
+                    let menu = Menu.fromMenuItem(id: index, item: item)
+                    menus.append(menu)
+                }
+                return menus
+            }
+            .take(1)
+            .bind(to: menuObservable)
+        
+    }
+    
+    func clearAllItemSelections() {
+        _ = menuObservable
+            .map { menus in
+                menus.map { m in
+                    Menu(id:m.id,name:m.name,price: m.price,count: 0)
+                }
+            }
+            .take(1) // 몇번 수행할지 정하는법 그래서 disposed가 필요없다.
+            .subscribe(onNext: {
+                self.menuObservable.onNext($0)
+            })
+            
+        
+    }
+    
+    func chagneCount(item : Menu, increase : Int) {
+        _ = menuObservable
+            .map { menus in
+                menus.map { m in
+                    if m.id == item.id {
+                        return Menu(id: m.id ,
+                                    name: m.name,
+                                    price: m.price,
+                                    count: max(m.count + increase, 0))
+                    } else {
+                        return Menu(id: m.id,
+                                    name: m.name,
+                                    price: m.price,
+                                    count: m.count)
+                    }
+                }
+            }
+            .take(1) // 몇번 수행할지 정하는법 그래서 disposed가 필요없다.
+            .subscribe(onNext: {
+                self.menuObservable.onNext($0)
+            })
+    }
+    
+    func Onorder() {
+        
+    }
     
 }
--- RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
+++ RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
@@ -12,29 +12,70 @@ import RxCocoa
 
 class MenuViewController: UIViewController {
     // MARK: - Life Cycle
-    
+    let cellID = "MenuItemTableViewCell"
     let viewModel = MenuListViewModel()
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        itemCountLabel.text = "\(viewModel.itemsCount)"
-        totalPrice.text = viewModel.totalPrice.currencyKR()
+
+        //Rxcocoa
+        //UIKit을 Rxswift에서 사용하기위해 extension한것이다.
+        
+//        viewModel.itemsCount
+//            .map{ "\($0)"}
+//            .subscribe(onNext: {
+//                self.itemCountLabel.text = $0
+//            })
+//            .disposed(by: disposeBag)
+        
+        // 위 코드를 사용하여 순환참조를 막으면서 줄수도 줄일수 있다
+        viewModel.itemsCount
+            .map{"\($0)"}
+            
+            .bind(to: itemCountLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.totalPrice
+            .map { $0.currencyKR() }
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.totalPrice.text = $0
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.menuObservable // 이렇게 작성시 데이터소스 부분이 필요가 없다.
+            .observeOn(MainScheduler.instance)
+            .bind(to: tableView.rx.items(cellIdentifier: cellID, cellType: MenuItemTableViewCell.self)) { index, item, cell in
+                cell.title.text = item.name
+                cell.price.text = "\(item.price)"
+                cell.count.text = "\(item.count)"
+                
+                cell.onChange = { [weak self] increase in
+                    self?.viewModel.chagneCount(item: item, increase: increase)
+                }
+
+            }
+            .disposed(by: disposeBag)
+        
+        
+        //이러한 일련의 과저을 스트림이라고한다.
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let identifier = segue.identifier ?? ""
-        if identifier == "OrderViewController",
-            let orderVC = segue.destination as? OrderViewController {
-            // TODO: pass selected menus
-        }
-    }
-
-    func showAlert(_ title: String, _ message: String) {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alertVC, animated: true, completion: nil)
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let identifier = segue.identifier ?? ""
+//        if identifier == "OrderViewController",
+//            let orderVC = segue.destination as? OrderViewController {
+//            // TODO: pass selected menus
+//        }
+//    }
+//
+//    func showAlert(_ title: String, _ message: String) {
+//        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        alertVC.addAction(UIAlertAction(title: "OK", style: .default))
+//        present(alertVC, animated: true, completion: nil)
+//    }
 
     // MARK: - InterfaceBuilder Links
 
@@ -44,29 +85,44 @@ class MenuViewController: UIViewController {
     @IBOutlet var totalPrice: UILabel!
 
     @IBAction func onClear() {
+        viewModel.clearAllItemSelections()
+        
     }
 
     @IBAction func onOrder(_ sender: UIButton) {
         // TODO: no selection
-        // showAlert("Order Fail", "No Orders")
-        performSegue(withIdentifier: "OrderViewController", sender: nil)
-    }
-}
-
-extension MenuViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.menus.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
+//        // showAlert("Order Fail", "No Orders")
+//        performSegue(withIdentifier: "OrderViewController", sender: nil)
+//        updateUI()
+//        viewModel.totalPrice += 100
+//        viewModel.totalPrice.onNext(100) // PublishSubject로 만들어서 해당 값을 넣어줄수 있다.
         
-        let menu = viewModel.menus[indexPath.row]
-
-        cell.title.text = menu.name
-        cell.price.text = "\(menu.price)"
-        cell.count.text = "\(menu.count)"
-
-        return cell
+//        viewModel.menuObservable.onNext([
+//            Menu(name: "change", price: Int.random(in: 100...1000), count: Int.random(in: 0...3)),
+//            Menu(name: "change", price: Int.random(in: 100...1000), count: Int.random(in: 0...3)),
+//            Menu(name: "change", price: Int.random(in: 100...1000), count: Int.random(in: 0...3)),
+//            Menu(name: "change", price: Int.random(in: 100...1000), count: Int.random(in: 0...3)),
+//        ])
+        viewModel.Onorder()
     }
+    
+//    func updateUI() {
+//        itemCountLabel.text = "\(viewModel.itemsCount)"
+//        totalPrice.text = viewModel.totalPrice.currencyKR()
+//    }
 }
+
+//extension MenuViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.menus.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
+//
+//        let menu = viewModel.menus[indexPath.row]
+//
+//
+//        return cell
+//    }
+//}
